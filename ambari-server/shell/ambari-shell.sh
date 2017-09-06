fee1c9fbb9a546836606430fee9930484b7f5e83
xuzhe <geniuszhe@163.com>
2017-09-06 13:00:47 +0800

    add hosts_gen

--- ambari-server/shell/ambari-shell.sh
+++ ambari-server/shell/ambari-shell.sh
@@ -6,5 +6,10 @@
 echo AMBARI_HOST=${AMBARI_HOST:? ambari server address is mandatory, fallback is a linked containers exposed 8080}
 
 ./wait-for-host-number.sh
+./hosts_gen.py
+
+curl -i -H "X-Requested-By: ambari" -u admin:admin -X PUT -d'{ "Repositories" : { "base_url":"http://10.0.83.123/HDP/2.x/updates/2.6.1.0/", "verify_base_url":true }}' http://localhost:8080/api/v1/stacks/HDP/versions/2.6/operating_systems/redhat7/repositories/HDP-2.6
+
+curl -i -H "X-Requested-By: ambari" -u admin:admin -X PUT -d'{"Repositories" : { "base_url":"http://10.0.83.123/HDP-UTILS-1.1.0.21/repos/centos7/","verify_base_url":true }}' http://localhost:8080/api/v1/stacks/HDP/versions/2.6/operating_systems/redhat7/repositories/HDP-UTILS-1.1.0.21
 
 java -jar ambari-shell.jar --ambari.host=$AMBARI_HOST
---
+++ ambari-server/shell/hosts_gen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import psycopg2
+import os
+conn = psycopg2.connect(database="ambari", user="ambari", password="bigdata", host="localhost", port="5432")
+
+print "Opened database successfully"
+
+cur = conn.cursor()
+
+cur.execute("select public_host_name,ipv4 from hosts")
+rows = cur.fetchall()
+host_file = "/etc/hosts"
+data=""
+with open(host_file, 'rb') as f:
+    data = f.read()
+    array = data.split("\n")
+    try:
+        search_start=array.index("# hosts begin")
+        search_end=array.index("# hosts end")
+        for i in range(search_start,search_end+1):
+            array.pop(search_start)
+        data = "\n".join(array)
+    except Exception as err:
+       print err
+with open(host_file,"w") as f:
+    f.write(data)
+    f.write("# hosts begin\n")
+    for row in rows:
+       f.write(row[1] + " " + row[0] + "\n")
+    f.write("# hosts end\n")
+conn.close
+
+scp_put = '''
+spawn scp %s %s@%s:%s
+expect "(yes/no)?" {
+send "yes\r"
+expect "password:"
+send "%s\r"
+} "password:" {send "%s\r"}
+expect eof
+exit'''
+for row in rows:
+    local_path="/etc/hosts"
+    user="root"
+    host=row[1]
+    remote_path="/etc"
+    pwd="bigdata"
+    os.system("echo '%s' > scp_put.cmd" % (scp_put % (os.path.expanduser(local_path), user, host, remote_path, pwd, pwd)))
+    os.system('expect scp_put.cmd')
+    os.system('rm scp_put.cmd')
